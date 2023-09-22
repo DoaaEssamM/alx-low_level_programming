709052bcbc59bc24b6cc595bca1c3021b3443831
DoaaEssamM <doaaessamm3@gmail.com>
2023-09-22 01:28:46 -0700

    dn

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,7 +8,7 @@
  * @str: string to put in the new node
  *
  * Return: address of the new element, or NULL if it failed
- */
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
@@ -27,13 +27,13 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->next = NULL;
 
 	if (*head == NULL)
-{
-	*head = new;
-	return (new);
-}
+	{
+		*head = new;
+		return (new);
+	}
 
-while (temp->next)
-	temp = temp->next;
+	while (temp->next)
+		temp = temp->next;
 
 	temp->next = new;
 
