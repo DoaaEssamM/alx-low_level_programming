c9c463518ca9ad1d2e068507caa74401ba96fc29
DoaaEssamM <doaaessamm3@gmail.com>
2023-09-25 08:59:19 -0700

    dn

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,31 +10,31 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp = *head;
+	listint_t *p = *head;
 	listint_t *current = NULL;
 	unsigned int i = 0;
-
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(temp);
-		return (1);
-	}
 
 	if (*head == NULL)
 		return (-1);
 
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(p);
+		return (1);
+	}
+
 	while (i < index - 1)
 	{
-		if (!temp || !(temp->next))
+		if (!p || !(p->next))
 			return (-1);
-		temp = temp->next;
+		p = p->next;
 		i++;
 	}
 
 
-	current = temp->next;
-	temp->next = current->next;
+	current = p->next;
+	p->next = current->next;
 	free(current);
 
 	return (1);
