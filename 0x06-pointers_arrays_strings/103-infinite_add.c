48ae5b276d49da577cc4fb87de78da48c71c74b5
DoaaEssamM <doaaessamm3@gmail.com>
2023-09-27 09:14:45 -0700

    dn

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,14 +2,14 @@
 
 /**
  * rev_string - reverse array
- * @n: int prams
+ * @n: integer params
  * Return: 0
  */
 
 void rev_string(char *n)
 {
 	int i = 0;
-	int k = 0;
+	int j = 0;
 	char temp;
 
 	while (*(n + i) != '\0')
@@ -18,26 +18,26 @@ void rev_string(char *n)
 	}
 	i--;
 
-	for (k = 0; k < i; k++, i--)
+	for (j = 0; j < i; j++, i--)
 	{
-		temp = *(n + k);
-		*(n + k) = *(n + i);
+		temp = *(n + j);
+		*(n + j) = *(n + i);
 		*(n + i) = temp;
 	}
 }
 
 /**
- * infinite_add - add 2 numbers
- * @n1: text for 1st number to add
- * @n2: text for 2nd number to add
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
  * @r: pointer to buffer
  * @size_r: buffer size
- * Return: pointer to call function
+ * Return: pointer to calling function
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, dig = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
@@ -63,16 +63,16 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			overflow = 1;
 		else
 			overflow = 0;
-		if (dig >= (size_r - 1))
+		if (digits >= (size_r - 1))
 			return (0);
-		*(r + dig) = (temp_tot % 10) + '0';
+		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		j--;
 		i--;
 	}
-	if (dig == size_r)
+	if (digits == size_r)
 		return (0);
-	*(r + dig) = '\0';
+	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
 }
